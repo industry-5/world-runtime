567f44518246f279e421efc15f49fee46e2ee1e9
Robb Bush <154751552+robb-at-design-science@users.noreply.github.com>
2026-03-22 18:14:49 -0500

    🔄

--- labs/world_game_studio_next/dist/src/services/authoringService.js
+++
@@ -1,7 +0,0 @@
-export function createAuthoringService(runtimeClient) {
-  return {
-    listTemplates() {
-      return runtimeClient.callRuntimeMethod("world_game.authoring.template.list", {});
-    },
-  };
-}
